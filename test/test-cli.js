e85d01f2e8b7dcf50a585f4f9eb9ad3ec2a78c27
keik <k4t0.kei@gmail.com>
2016-11-16 08:35:56 +0900

    Add tests

---
+++ test/test-cli.js
@@ -0,0 +1,30 @@
+var test = require('tape')
+
+var concat = require('concat-stream')
+var fs = require('fs')
+var path = require('path')
+var through = require('through2')
+
+var spawn = require('child_process').spawn
+
+test('`watch-test` with no options should show usage', function(t) {
+  spawn('./bin/watch-test').stdout
+    .pipe(concat(function(buf) {
+      fs.readFile('./bin/usage', function(e, data) {
+        if (e) throw new Error(e)
+          t.is(buf.toString(), data.toString())
+        t.end()
+      })
+    }))
+})
+
+test('`watch-test` with `-h` options should show usage', function(t) {
+  spawn('./bin/watch-test', ['-h', 'foo', 'bar']).stdout
+    .pipe(concat(function(buf) {
+      fs.readFile('./bin/usage', function(e, data) {
+        if (e) throw new Error(e)
+          t.is(buf.toString(), data.toString())
+        t.end()
+      })
+    }))
+})
